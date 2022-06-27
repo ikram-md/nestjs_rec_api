93f607fb43e73a489a7c8e79fcfc7ab075a4dae2
ikram <jazzthedreameryeah@gmail.com>
2022-06-27 21:53:20 +0100

    implemented a get request

--- src/clients/controllers/clients/clients.controller.ts
+++ src/clients/controllers/clients/clients.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ClientsService } from 'src/clients/services/clients/clients.service';
 
 @Controller('clients')
 export class ClientsController {
   //injecting the client service in the customers list controller
   constructor(private clientService: ClientsService) {}
-  @Get('')
-  getCustomersList() {
-    return this.clientService.findOneClient();
+  @Get('/search/:id')
+  searchForClient(@Param('id', ParseIntPipe) id: number) {
+    const foundClient = this.clientService.findOneClient(id);
+    if (foundClient) return foundClient;
+    else throw new HttpException('Client was not found ', HttpStatus.NOT_FOUND);
   }
 }
--- src/clients/services/clients/clients.service.ts
+++ src/clients/services/clients/clients.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ClientsService {
-  findOneClient() {
-    return { id: 1, email: 'ikr@gmail.com', password: 'version555' };
+  users = [
+    { id: 2, email: 'ikr@gmail.com', password: 'version555' },
+    { id: 1, email: 'dogs@gmail.com', password: 'version554' },
+    { id: 3, email: 'cats@gmail.com', password: 'version553' },
+  ];
+  findOneClient(id: number) {
+    return this.users.find((u) => u.id === id);
   }
 }
