7c2f23e52c3ab06a68857b3d34fa6ed704006e94
ikram <jazzthedreameryeah@gmail.com>
2022-06-29 16:31:19 +0100

    automatic import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientsController } from './clients/controllers/clients/clients.controller';
 import { ClientsService } from './clients/services/clients/clients.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
