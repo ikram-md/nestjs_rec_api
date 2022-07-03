2939d0407236947fff278824019a27957822aab6
ikram <jazzthedreameryeah@gmail.com>
2022-07-03 19:44:33 +0100

    added hashing password hook

--- src/typeorm/entities/User.ts
+++ src/typeorm/entities/User.ts
@@ -1,5 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn({ type: 'bigint', name: 'user_id' })
@@ -13,4 +13,9 @@ export class User {
 
   @Column({ name: 'email_address', default: '' })
   email: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 }
