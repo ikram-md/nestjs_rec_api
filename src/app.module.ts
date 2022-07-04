3b39cc3ce0016521c8916e3b8808de342d05cbee
ikram <jazzthedreameryeah@gmail.com>
2022-07-04 16:59:58 +0100

    fixed dependency error

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { ClientsController } from './clients/controllers/clients/clients.control
 import { ClientsService } from './clients/services/clients/clients.service';
 import entities from './typeorm/index';
 import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { UsersController } from './users/controller/users/users.controller';
+import { UsersService } from './users/services/users/users.service';
+import { AuthenticationService } from './authentication/services/authentication.service';
+import { AuthenticationController } from './authentication/controllers/authentication.controller';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv');
 dotenv.config();
 @Module({
   imports: [
     UsersModule,
+    AuthenticationModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       port: 3306,
@@ -20,6 +26,7 @@ dotenv.config();
       synchronize: true,
       logging: true,
     }),
+    AuthenticationModule,
   ],
   controllers: [ClientsController],
   providers: [ClientsService],
