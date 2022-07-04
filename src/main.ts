adcc69962b6825654bd4f05112bcfd634f12793e
ikram <jazzthedreameryeah@gmail.com>
2022-07-04 17:00:28 +0100

    session config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
+import * as passport from 'passport';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(
+    session({
+      secret: `${process.env.SESSION_SECRET}`,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+        secure: true,
+      },
+    }),
+  );
   await app.listen(8000);
 }
 bootstrap();
