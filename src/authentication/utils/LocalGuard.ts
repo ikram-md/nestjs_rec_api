41cf617d52edc3bd010785461488f765ac8508de
ikram <jazzthedreameryeah@gmail.com>
2022-07-04 23:08:58 +0100

    added auth protection guard logic

--- src/authentication/utils/LocalGuard.ts
+++ src/authentication/utils/LocalGuard.ts
@@ -1,12 +1,26 @@
-import { ExecutionContext, UseGuards } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
+import { Request } from 'express';
 
+@Injectable()
 export class LocalGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext) {
     const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
     await super.logIn(request);
     return result;
+  }
+}
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<any> {
+    const req = context.switchToHttp().getRequest<Request>();
+    return req.isAuthenticated();
   }
 }
