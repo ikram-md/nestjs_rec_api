1f0e802eba3558bc275f40aa4ece36c70899e2fc
ikram <jazzthedreameryeah@gmail.com>
2022-07-04 17:00:40 +0100

    jwt token config

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
@@ -7,9 +8,18 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthenticationController } from './controllers/authentication.controller';
 import { AuthenticationService } from './services/authentication.service';
 import { LocalStrategy } from './utils/LocalStrategy';
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UsersModule, PassportModule],
+  imports: [
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: { expiresIn: '1200s' },
+    }),
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+    PassportModule,
+  ],
   controllers: [AuthenticationController],
   providers: [
     { provide: 'AUTH_SERVICE', useClass: AuthenticationService },
@@ -18,6 +28,7 @@ import { LocalStrategy } from './utils/LocalStrategy';
       useClass: UsersService,
     },
     LocalStrategy,
+    JwtService,
   ],
 })
 export class AuthenticationModule {}
