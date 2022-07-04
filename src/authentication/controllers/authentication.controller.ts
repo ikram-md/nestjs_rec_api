577dd766598729b57f9416bfa72f34eb5a7bd5a6
ikram <jazzthedreameryeah@gmail.com>
2022-07-04 22:48:48 +0100

    injected local guard

--- src/authentication/controllers/authentication.controller.ts
+++ src/authentication/controllers/authentication.controller.ts
@@ -4,20 +4,26 @@ import {
   Injectable,
   Post,
   Request,
+  Session,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthenticationService } from '../services/authentication.service';
+import { LocalGuard } from '../utils/LocalGuard';
 @Injectable()
 @Controller('auth')
 export class AuthenticationController {
   constructor(
     @Inject('AUTH_SERVICE') private authService: AuthenticationService,
   ) {}
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalGuard)
   @Post('login')
-  async logIn(@Request() req) {
-    return this.authService.login(req.user);
+  async logIn(@Request() req, @Session() session: Record<string, any>) {
+    req.session.user = req.user;
+    return {
+      token: await this.authService.login(req.user),
+      session: session.id,
+    };
   }
   //implement the sign up controller
 }
